9641a8c30754f9bdf778c1e3e3fbcae119597ba8
jasmeet17 <jasmeet17mast@gmail.com>
2018-06-13 16:58:55 -0700

    Add Constructor and AutoWired Annotation for Dependency Injection

--- spring-demo-annotation/src/com/spring/demo/TennisCoach.java
+++ spring-demo-annotation/src/com/spring/demo/TennisCoach.java
@@ -1,13 +1,21 @@
 package com.spring.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component("thatSillyCoach")
 public class TennisCoach implements Coach {
+	
+	private FortuneService fortuneServices;
+	
+	@Autowired
+	public TennisCoach(FortuneService fortuneServices) {
+		this.fortuneServices = fortuneServices;
+	}
 
 	@Override
 	public String getDailyWorkout() {
-		return "Practice Backhand";
+		return fortuneServices.getFortune();
 	}
 
 }

85a5898c0c7e5a09b0065ac2c676c7011651f1f5
jasmeet17 <jasmeet17mast@gmail.com>
2018-06-19 21:00:53 -0700

    Creating and saving Java objects using hibernate

---
+++ hibernate-tutorial/src/com/luv2code/hibernate/demo/CreateStudentDemo.java
@@ -0,0 +1,49 @@
+package com.luv2code.hibernate.demo;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.luv2code.hibernate.demo.entity.Student;
+
+public class CreateStudentDemo {
+
+	public static void main(String[] args) {
+		
+		// Create Session Factory
+		SessionFactory factory = new Configuration()
+				.configure("hibernate.cfg.xml")
+				.addAnnotatedClass(Student.class)
+				.buildSessionFactory();
+
+		System.out.println("Creating new Object");
+		
+		// Create session
+		Session session = factory.getCurrentSession();
+		
+		try {
+			//use session to save Java Object
+			
+			//create a student object
+			System.out.println("Creating new Object");
+			Student tempStudent = new Student("Paul","Smith","paulsmith@gmail.com");
+			
+			//start a transaction
+			System.out.println("Begin Transaction");
+			session.beginTransaction();
+			
+			//save the student object
+			System.out.println("Save object");
+			session.save(tempStudent);
+			
+			//commit transaction
+			System.out.println("Commit Transaction");
+			session.getTransaction().commit();
+			
+		} finally {
+			factory.close();
+		}
+		
+	}
+
+}

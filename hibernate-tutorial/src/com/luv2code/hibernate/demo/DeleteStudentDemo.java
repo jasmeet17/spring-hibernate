765292bd8ad133ed6e4085376770d76e0cdd1d1a
jasmeet17 <jasmeet17mast@gmail.com>
2018-06-20 15:43:53 -0700

    Delete record from DB

---
+++ hibernate-tutorial/src/com/luv2code/hibernate/demo/DeleteStudentDemo.java
@@ -0,0 +1,48 @@
+package com.luv2code.hibernate.demo;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.luv2code.hibernate.demo.entity.Student;
+
+public class DeleteStudentDemo {
+
+	public static void main(String[] args) {
+		
+		// Create Session Factory
+		SessionFactory factory = new Configuration()
+				.configure("hibernate.cfg.xml")
+				.addAnnotatedClass(Student.class)
+				.buildSessionFactory();
+
+		System.out.println("Creating new Object");
+		
+		// Create session
+		Session session = factory.getCurrentSession();
+		
+		try {
+			
+			//start a transaction
+			System.out.println("Begin Transaction");
+			session.beginTransaction();
+			
+			// get student based on primary key
+			Student myStudent = session.get(Student.class, 1);
+			
+			
+			//Delete the student object
+			System.out.println("Delete . . .");
+			session.delete(myStudent);
+			
+			//commit transaction
+			System.out.println("Commit Transaction");
+			session.getTransaction().commit();
+			
+		} finally {
+			factory.close();
+		}
+		
+	}
+
+}
